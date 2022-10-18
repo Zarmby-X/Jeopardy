e9a61dd3ddc9cecc2580ffcf7327700b04024456
Zarmby-X <Armando.hernandez.r@hotmail.com>
2022-10-18 15:59:56 -0500

    up project commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Configuration from './Components/Configuration/Configuration';
+
+function App() {
+  return (
+   <div>
+      <Configuration></Configuration>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Configuration/Configuration.jsx
@@ -0,0 +1,12 @@
+import "./Configuration.scss"
+
+let Configuration = () => {
+    return (
+        <div id="test">
+            hola
+        </div>
+    )
+}
+
+export default Configuration;
+
